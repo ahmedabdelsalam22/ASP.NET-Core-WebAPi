904f948dab13d59debb8a59317e633075f80436e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-10 21:17:37 +0300

    add connectionString

---
+++ WebAPi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPi.Models;
+
+namespace WebAPi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
