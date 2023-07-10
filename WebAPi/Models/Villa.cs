d4c5d595a0b118e60c3031742530c2511c53ad0a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-10 21:06:49 +0300

    refactor villa and villaDto models

--- WebAPi/Models/Villa.cs
+++ WebAPi/Models/Villa.cs
@@ -1,9 +1,20 @@
-﻿namespace WebAPi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPi.Models
 {
     public class Villa
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Required]
+        public String Name { get; set; }
+        public String Details { get; set; }
+        public int Occupancy { get; set; }
+        public int Sqft { get; set; }
+        public double Rate { get; set; }
+        public String ImageUrl { get; set; }
+        public String Amenity { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
--- WebAPi/Program.cs
+++ WebAPi/Program.cs
@@ -2,8 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-
+builder.Services.AddControllers(opt => 
+{
+   // opt.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters(); // add xml formater
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
