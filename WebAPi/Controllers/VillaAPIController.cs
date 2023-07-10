feb0e5162a01fc49b5a1441dc329c4d7abaa85ae
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-10 13:01:09 +0300

    first controller and endpoint

---
+++ WebAPi/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPi.Models;
+
+namespace WebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> 
+            {
+                new Villa {Id = 1,Name = "Villa1"},
+                new Villa {Id = 2,Name = "Villa2"},
+            };              
+        }
+    }
+}
