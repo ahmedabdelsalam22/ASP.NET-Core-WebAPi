0db733195f2c4f694f2e22028b701a73399efdb1
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-10 13:09:06 +0300

    DTOs

--- WebAPi/Controllers/VillaAPIController.cs
+++ WebAPi/Controllers/VillaAPIController.cs
@@ -1,20 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebAPi.Models;
+using WebAPi.Models.DTO;
 
 namespace WebAPi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [Route("api/[Controller]")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa> 
+            return new List<VillaDTO> 
             {
-                new Villa {Id = 1,Name = "Villa1"},
-                new Villa {Id = 2,Name = "Villa2"},
+                new VillaDTO {Id = 1,Name = "Villa1"},
+                new VillaDTO {Id = 2,Name = "Villa2"},
             };              
         }
     }
