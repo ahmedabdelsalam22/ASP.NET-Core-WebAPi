6c27bd9fb4601b14c03b97c2beac7756f6cb92a1
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-10 21:10:47 +0300

    install needed packages

--- WebAPi/Models/Villa.cs
+++ WebAPi/Models/Villa.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebAPi.Models
 {
     public class Villa
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         [Required]
         public String Name { get; set; }
