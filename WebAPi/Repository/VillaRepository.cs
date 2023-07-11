e316366897bbb96412e59948b6cf74ffbc56d21a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-11 15:16:05 +0300

    create Generic Repository

--- WebAPi/Repository/VillaRepository.cs
+++ WebAPi/Repository/VillaRepository.cs
@@ -10,59 +10,7 @@ namespace WebAPi.Repository
     public class VillaRepository : IVillaRepository
     {
         private readonly ApplicationDbContext _db;
-        private readonly DbSet<Villa> _dbset;
 
-        public VillaRepository(ApplicationDbContext db)
-        {
-            _db = db;
-            _dbset = _db.Set<Villa>();
-        }
 
-        public async Task CreateAsync(Villa entity)
-        {
-            await _dbset.AddAsync(entity);
-            await SaveAsync();
-        }
-
-        public async Task UpdateAsync(Villa entity)
-        {
-            _dbset.Update(entity);
-            await SaveAsync();
-        }
-
-        public async Task<Villa> GetAsync(Expression<Func<Villa,bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Villa> query = _dbset;
-            if (!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-            if (filter != null)
-            {
-                query = query.Where(filter);
-            }
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Villa>> GetAllAsync(Expression<Func<Villa,bool>> filter = null)
-        {
-            IQueryable<Villa> query = _dbset;
-            if(filter != null)
-            {
-                query = query.Where(filter);
-            }
-            return await query.ToListAsync();
-        }
-
-        public async Task RemoveAsync(Villa entity)
-        {
-             _dbset.Remove(entity);
-             await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
-            await _db.SaveChangesAsync();
-        }
     }
 }
