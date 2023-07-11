4bf1e31d2a98b20cb65551dd4608ee456177aa35
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-11 16:27:26 +0300

    add try..catch in GetVillas endpoint

--- WebAPi/Controllers/VillaAPIController.cs
+++ WebAPi/Controllers/VillaAPIController.cs
@@ -29,15 +29,24 @@ namespace WebAPi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<APIResponse>> GetVillas()
         {
-            IEnumerable<Villa> villas = await _repository.GetAllAsync();
+            try
+            {
+                IEnumerable<Villa> villas = await _repository.GetAllAsync();
 
-            var villaDTO = _mapper.Map<IEnumerable<VillaDTO>>(villas);
+                var villaDTO = _mapper.Map<IEnumerable<VillaDTO>>(villas);
 
-            _response.Result = villaDTO;
-            _response.IsSuccess = true;
-            _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = villaDTO;
+                _response.IsSuccess = true;
+                _response.StatusCode = HttpStatusCode.OK;
 
-            return Ok(_response);           
+                return Ok(_response);
+            }
+            catch (Exception e) 
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string>() { e.ToString() };
+            }
+            return _response;
         }
         [HttpGet("{id}",Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -70,7 +79,7 @@ namespace WebAPi.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<ActionResult<VillaDTO>> Create([FromBody] VillaCreateDTO villaCreateDTO)
+        public async Task<ActionResult<APIResponse>> Create([FromBody] VillaCreateDTO villaCreateDTO)
         {
             if (await _repository.GetAsync(filter: x => x.Name.ToLower() == villaCreateDTO.Name.ToLower()) != null)
             {
@@ -85,14 +94,19 @@ namespace WebAPi.Controllers
             Villa villa = _mapper.Map<Villa>(villaCreateDTO);
 
             await _repository.CreateAsync(villa);
+            _response.Result = villa;
+            _response.IsSuccess = true;
+            _response.StatusCode = HttpStatusCode.Created;
 
+            //return CreatedAtRoute("GetVilla", new { id = villa.Id}, villa);
+            return Ok(_response);
         }
 
 
         [HttpPut(Name ="UpdateVilla")]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> Update(int id, [FromBody]VillaDTO villaDTO)
+        public async Task<ActionResult<APIResponse>> Update(int id, [FromBody]VillaDTO villaDTO)
         {
             if (villaDTO == null || id != villaDTO.Id)
             {
@@ -103,7 +117,10 @@ namespace WebAPi.Controllers
 
             await _repository.UpdateAsync(villa);
 
-            return NoContent();
+            _response.StatusCode = HttpStatusCode.NoContent;
+            _response.IsSuccess= true;
+
+            return Ok(_response);
         }
 
 
@@ -123,7 +140,10 @@ namespace WebAPi.Controllers
             }
             await _repository.RemoveAsync(villa);
 
-            return NoContent();
+            _response.StatusCode = HttpStatusCode.NoContent;
+            _response.IsSuccess = true;
+
+            return Ok(_response);
         }
     }
 }
