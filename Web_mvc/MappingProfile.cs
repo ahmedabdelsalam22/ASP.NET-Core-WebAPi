6cb11fd3b8c9f8438bdd19aceb179504517e61b8
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-12 14:21:49 +0300

    install autoMapper packages and create Mapping class and register it in program cs

---
+++ Web_mvc/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Web_mvc.Models.DTO;
+
+namespace Web_mvc
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<VillaDTO,VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO,VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO,VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO,VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
--- Web_mvc/Program.cs
+++ Web_mvc/Program.cs
@@ -1,7 +1,11 @@
+using Web_mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 
 var app = builder.Build();
 
