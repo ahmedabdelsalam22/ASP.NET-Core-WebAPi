f2a3a060616cc883322cc21e461fbdf4405a96e5
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-14 21:08:15 +0300

    create VillaController and getting all villas

---
+++ Web_mvc/Controllers/VillaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Web_mvc.Models;
+using Web_mvc.Models.DTO;
+using Web_mvc.Services.IServices;
+
+namespace Web_mvc.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _service;
+        public VillaController(IVillaService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new();
+
+            var response = await _service.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess) 
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
