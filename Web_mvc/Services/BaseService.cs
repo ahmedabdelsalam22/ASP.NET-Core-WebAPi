dc844dfac9a04bd6cddf529014e4b87e7cf40824
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-14 19:21:15 +0300

    create IBaseService

---
+++ Web_mvc/Services/BaseService.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Unicode;
+using Web_mvc.Models;
+using Web_mvc.Services.IServices;
+using static Web_Utility.SD;
+
+namespace Web_mvc.Services
+{
+    public class BaseService : IBaseService
+    {
+        public IHttpClientFactory httpClient { get; set; }
+        public APIResponse responseModel { get ; set ; }
+
+        public BaseService(IHttpClientFactory _httpClient)
+        {
+            _httpClient = httpClient;
+            responseModel = new ();
+        }
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MagicVilla");
+
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if (apiRequest.Data != null)
+                {
+                    message.Content =
+                        new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+                switch (apiRequest.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+
+                }
+
+                HttpResponseMessage apiResponse = null;
+
+
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch (Exception e)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessage = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
+            }
+        }
+    }
+}
