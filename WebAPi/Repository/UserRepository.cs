b6f1382e956bba11c06da4fa6e8a8c7e10f94567
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-07-22 20:08:43 +0300

    IUserRepository

---
+++ WebAPi/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using WebAPi.Data;
+using WebAPi.Models;
+using WebAPi.Models.DTO;
+using WebAPi.Repository.IRepository;
+
+namespace WebAPi.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public UserRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
